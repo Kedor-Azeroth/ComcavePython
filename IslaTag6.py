6f4e4f8536a9d5cfeecb8960fd5cbf30513d7353
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-20 11:00:03 +0200

    input

--- IslaTag6.py
+++ IslaTag6.py
@@ -4,6 +4,9 @@ print('APFEL' .count ('P'))  # Ausgabe: 1
 
 Mexiko = ['Mexiko', 'Deutschland', 'Brasilien', 'Argentinien', 'Frankreich']
 print(Mexiko.count('Mexiko'))  # Ausgabe: 1
+# Einzeilig Kommentare
+''' Mehrzeilige Kommentare'''
+
 
 ''' Fussball Beispiel'''
 # Pkt -> erreichte Punkte (S*3 + U*1 + N*0)
@@ -12,4 +15,25 @@ print(Mexiko.count('Mexiko'))  # Ausgabe: 1
 # U -> Anzahl der Unentschieden (U = 1 Punkt)
 # N -> Anzahl der Niederlagen (N = 0 Punkte)
 #
+''' EVA-Prinzip:
+ Eingabe:
+1) Anzahl der Siege (S)
+2) Anzahl der Unentschieden (U)
+3) Anzahl der Niederlagen (N)
+ 
+ Verarbeitung:
+1) Berechnung der Punktezahl  (Pkt = S*3 + U*1 + N*0)
+2) Berechnung der absolvierten Begegnungen (AB = S+U+N)  
+
+Ausgabe:
+1) Informative Darstellung der ermittleten Werte 
+
+
+
+''' 
+
+anz_siege = int(input('Anzahl der Siege: '))
+anz_unentschieden = int(input('Anzahl der Unentschieden: '))
+anz_niederlagen = int(input('Anzahl der Niederlagen: '))
+
 
