783f41a0b4465a8662fe28a9991740e057fb94e8
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-20 10:42:51 +0200

    Tag6 Fussballspiel

--- IslaTag6.py
+++ IslaTag6.py
@@ -2,3 +2,14 @@
 print('APFEL' .count ('P'))  # Ausgabe: 1
 
 
+Mexiko = ['Mexiko', 'Deutschland', 'Brasilien', 'Argentinien', 'Frankreich']
+print(Mexiko.count('Mexiko'))  # Ausgabe: 1
+
+''' Fussball Beispiel'''
+# Pkt -> erreichte Punkte (S*3 + U*1 + N*0)
+# AB -> Anzahl der absolvierten Spiele (S+U+N)
+# S -> Anzahl der Siege (S = 3 Punkte)
+# U -> Anzahl der Unentschieden (U = 1 Punkt)
+# N -> Anzahl der Niederlagen (N = 0 Punkte)
+#
+
