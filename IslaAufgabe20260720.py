f39706520ee893fbd151c42ecde01d7e3fe6c987
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-20 14:52:20 +0200

    Aufgabe Temperatur

--- IslaAufgabe20260720.py
+++ IslaAufgabe20260720.py
@@ -1,4 +1,23 @@
-'''Aufgabe                  '''
+'''Aufgabe Umrechnung Grad Celsius in 1) Grad Fahrenheit 2) Kelvin    '''
+#Eingabe:
+
+
+umrechnung = float(input('Bitte geben Sie einen Wert ein: '))
+#umrechnung_kelvin = float(input('Bitte geben Sie einen Wert ein: '))
+
+# Verarbeitung
+fahrenheit = umrechnung * 9 / 5 + 32
+kelvin = umrechnung + 273.15
+
+
+# Ausgabe
+print('Temperatur in Fahrenheit ist:', fahrenheit)
+print('Temperatur in Kelvin ist:', kelvin)
+
+
+
+
+
 '''Aufgabe                  '''
 '''Aufgabe                  '''
 '''Aufgabe                  '''
