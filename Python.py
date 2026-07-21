f27bdaeca83147a4c0b5fe7e3aed0706805b673a
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-21 13:39:40 +0200

    try

--- Python.py
+++ Python.py
@@ -1,16 +1,8 @@
 'W3Schools.com'
-#03// Hier beginnt der Hauptinhalt der Seite hierfehlt eine navbar für den Privaten Bereich, 
-#die den Benutzer zu den verschiedenen Themenbereichen führt. Die Navigation sollte klar und einfach zu bedienen sein, 
-#um eine gute Benutzererfahrung zu gewährleisten.
-print(9+2)
-
-#print(type("\"Hallo\""))
-
-
 # Case Sensitivity¡!!!!!!!!!
 # Duck-Typing erkennt selbst Datentypen und wandelt sie automatisch um, wenn nötig.
-#
-
+print(9+2)
+#print(type("\"Hallo\""))
 #print("Drucken"," Hier") #   >Ausgabe Konsole  Drucken hier
 #Print("Drucken") #> Drucken
 #Print()          #> es wird eine Zeile eingeschoben ohne ausgabe  
@@ -19,15 +11,12 @@ print(9+2)
 #name = input("Wie heißt du? ") #Zahl = 4        # Variable Zahl mit dem Wert 4
 #Zahl = 4        # Variable Zahl mit dem Wert 4
 #print(name + " will Python lernen " + str(Zahl) + " in Wochen")  # Nun mit variablen  str als int in String umwandeln
-
-
 #print(name + " will Python lernen " + str(Zahl) + " in Wochen")  # Nun mit variablen  str als int in String umwandeln
 #print("ich will Python lernen in",4,"Wochen")
 #item = "Bike" """Bike steht hier als Varible für Item
 #                 Variable Box: Item Variablenwert "Bike" mit Informationen 
 #                 Variablen speichern Informationen die sich ändern können (Fortschritt eines Spiels)"""
 #print("Ich habe ein", item)  ###> Ich habe ein Bike
-
 #print(9+2)  ist mit /*  -  +  möglich
 #Modulo %  Rest einer Division Beispiel 7/3 = 2 Rest 1
 #print(9%2) #> 1  Rest der Division von 9 durch 2 ist Ergebniss 1 
@@ -35,9 +24,6 @@ print(9+2)
 #del  y löscht die Variable y und gibt den Speicherplatz frei
 #type() gibt den Datentyp der Variable aus
 # x = True  # Boolean True oder False
-
-
-
 """ Comment 1 Zeile oder mehrzeilig mit drei Anführungszeichen
 Dieses beeinflusst auch nicht den Code, es ist nur eine Anmerkung für den Programmierer, damit er sich besser zurechtfindet. 
 Es könnte auch eine To-Do Liste
@@ -73,3 +59,136 @@ Es könnte auch eine To-Do Liste
 # a = int('3') *3 string wird in int umgewandelt und mit 3 multipliziert
 # dauer_stunde = round ((geh_zeit + jogging_zeit + schwimm_zeit )/60, 2)             # gerundet 
 #round(wandflaeche_gesamt * 0.15, 1)  # 0.15 Liter pro m²
+#print(round('Temperatur in Kelvin ist:', kelvin,2))
+#print(round(kelvin, 3), 'K')
+#\n zeitlen umbruch ('\n Das Program')
+'''Error Handling in Python   #Nach if, elif, else, try und except steht immer ein Doppelpunkt:
+try:                        # bei fehler try block sonst else Block es kommt aufjeden Finallyblock
+    # Code, der einen Fehler verursachen kann
+    zahl = int(input("Zahl eingeben: "))
+
+except ValueError:
+    # Fehler: Es wurde keine ganze Zahl eingegeben
+    print("Es wurde keine gültige Zahl eingegeben.")
+
+except KeyboardInterrupt:
+    # Fehler: Benutzer hat das Programm mit Strg + C beendet
+    print()
+    print("Das Programm wurde vom Benutzer abgebrochen.")
+    
+    try:weitere Handlings möglich
+    except
+    else
+    finally
+
+else:
+    # Wird nur ausgeführt, wenn kein Fehler aufgetreten ist
+    print("Vielen Dank.")
+
+finally:                                 #wird immer ausgeführt
+    # Wird immer ausgeführt
+    print("Auf Wiedersehen.")'''
+#try: → Hier steht der Code, der einen Fehler verursachen könnte.
+#except: → Fängt einen bestimmten Fehler ab und behandelt ihn.
+#else: → Wird nur ausgeführt, wenn im try-Block kein Fehler aufgetreten ist.
+#finally: → Wird immer ausgeführt, unabhängig davon, ob ein Fehler aufgetreten ist oder nicht
+#try
+# │
+# ├── Fehler? ── Ja ──► except
+# │
+# └── Nein ───────────► else
+#                      │
+#                      ▼
+#                   finally
+#try = ausprobieren → except = Fehler behandeln → else = kein Fehler → finally = immer ausführen.
+#Beispiel zum Anfangen
+'''Getraenkemarkt'''
+try:
+#Eingabe
+    bierkisten = int(input('Anzahlkisten:'))
+    wasserkisten = int(input('Anzahl Wasser-Kisten:'))
+    einzelflaschen = int(input('Anzahl einzelne Flaschen'))
+except ValueError:
+    print('nein')
+except KeyboardInterrupt:
+    print('Programm wurde beendet')    
+else:
+   print( 'Schön')
+
+#Verarbeitung
+
+#Ausgabe
+
+#Beispiel zum Anfangen von Fehlern
+'''Getraenkemarkt
+try:
+#Eingabe
+    bierkisten = int(input('Anzahlkisten:'))
+    wasserkisten = int(input('Anzahl Wasser-Kisten:'))
+    einzelflaschen = int(input('Anzahl einzelne Flaschen'))
+except ValueError:
+    print('nein')
+else:
+   #wenn eine der 3 Variablen negativ ist Hinweise an Nutzer, ansonsten alles ganz normal 
+   
+   if bierkisten <= 0 or wasserkisten <= 0 or einzelflaschen  <= 0:
+       print('positive Zahlen')
+   else:
+#Verarbeitung
+    gesamt_flaschen = bierkisten *  + wasserkisten *  + einzelflaschen
+    gesamt_pfand = gesamt_flaschen * 0.25
+
+#Ausgabe
+    print('Gesamtzahl Flaschen:', gesamt_flaschen)
+    print('Gesamtpfand:' ,gesamt_pfand, 'Euro')'''
+#Alternative
+'''Getraenkemarkt
+try:
+#Eingabe Eingabe wird bei Falsch abgebrochen.
+    bierkisten = int(input('Anzahlkisten:'))
+    if bierkisten <= 0:
+        raise ValueError
+        
+    wasserkisten = int(input('Anzahl Wasser-Kisten:'))
+    if wasserkisten <= 0:
+        raise ValueError
+    einzelflaschen = int(input('Anzahl einzelne Flaschen'))
+    if einzelflaschen <= 0:
+        raise ValueError
+except ValueError:
+    print('nein')
+except KeyboardInterrupt:
+       print('')    
+else:
+   #Verarbeitung
+    gesamt_flaschen = bierkisten *  + wasserkisten *  + einzelflaschen
+    gesamt_pfand = gesamt_flaschen * 0.25
+
+#Ausgabe
+    print('Gesamtzahl Flaschen:', gesamt_flaschen)
+    print('Gesamtpfand:' ,gesamt_pfand, 'Euro')'''
+
+
+'''
+If, elif, else   # Verzeigung von Bedingungen
+
+[Start]
+   |
+   v
+[ if Bedingung 1 ? ] --(Wahr)--> [Block 1 ausführen] --> [Ende]
+   | (Falsch)
+   v
+[ elif Bedingung 2 ? ] --(Wahr)--> [Block 2 ausführen] --> [Ende]
+   | (Falsch)
+   v
+[ ... ]
+   | (Falsch)
+   v
+[ else ] --> [Standard-Block ausführen] --> [Ende]
+
+Einfache IF Vergleiche
+
+if 2 == 3:
+    print("Die Zahlen sind gleich")
+
+    '''
