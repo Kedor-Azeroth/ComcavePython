da32c2c5e4e98c881712c3ddf06ffa552b7b0c15
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-23 13:56:00 +0200

    9 & Return x fertig

--- Projektnotenverwaltung.py
+++ Projektnotenverwaltung.py
@@ -16,6 +16,8 @@
 #
 import os
 
+punkte_liste = [[] for _ in range(10)]  # Index 0 = Schüler 1, ... Index 9 = Schüler 10
+
 '''Notenverwaltung'''
 while True:
     #Hauptmenü
@@ -37,59 +39,185 @@ while True:
     print('[9] Programm beenden')
     auswahl = input('>>> Ihre Eingabe:  ')   #Eingabe erforderlich
 
-    if   auswahl == '1':
+    if auswahl == '1':
         while True:
             print()
-            print('"Du bist im Menü "Punkte eingeben"')
-            print()
-            break
-        
+            print('Du bist im Menü "Punkte eingeben"')
+            print('[x] Zum Hauptmenü')
+            eingabe = input('>>> Schüler-ID eingeben (1-10) oder x für Hauptmenü: ')
+
+            if eingabe.lower() == 'x':
+                break
+
+            try:
+                schueler_id = int(eingabe)
+                if not (1 <= schueler_id <= 10):
+                    print('Fehler: Schüler-ID muss zwischen 1 und 10 liegen.')
+                    input('Weiter mit Enter...')
+                    continue
+            except ValueError:
+                print('Fehler: Bitte eine ganze Zahl zwischen 1 und 10 eingeben.')
+
+                input('Weiter mit Enter...')
+                continue
+
+            try:
+                punkte_eingabe = float(input('Wie viele Punkte möchten Sie eingeben (0-100)? '))
+                if not (0 <= punkte_eingabe <= 100):
+                    print('ERROR: Wert wird nicht übernommen, er liegt nicht im Wertebereich von 0 - 100.')
+                    input('Weiter mit Enter...')
+                    continue
+            except ValueError:
+                print('Bitte nur Dezimalzahlen 1-100 im Format 10.1 eingeben.')
+                input('Weiter mit Enter...')
+                continue
+
+            punkte_liste[schueler_id - 1].append(punkte_eingabe)
+            print(f'{punkte_eingabe} Punkte wurden Schüler {schueler_id} zugeordnet.')
+            input('Weiter mit Enter...')
+
     elif auswahl == '2':
         while True:
                     print()
                     print('"Du bist im Menü "Alle Punkte anzeigen"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+
+                    if auswahl == 'x':
+                         print('Du kehrst zum Hauptmenü zurück.')
+
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                      os.system("pause")
+                               
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe')
+
+                    
 
     elif auswahl == '3':
         while True:
                     print()
                     print('"Du bist im Menü "Durchschnitt berechen"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
-        
+                    print('Ungültige Eingabe')
+                    
     elif auswahl == '4':
         while True:
                     print()
                     print('"Du bist im Menü "Note aus Punktezahl ermitteln"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe')                   
         
     elif auswahl == '5':
         while True:
                     print()
                     print('"Du bist im Menü "Min, Max Auswertung"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe')                    
 
     elif auswahl == '6':
         while True:
                     print()
                     print('"Du bist im Menü "Notenspiegel"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe') 
+
     elif auswahl == '7':
         while True:
                     print()
                     print('"Du bist im Menü "Punkte löschen"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe')
+                  
     elif auswahl == '8':
         while True:
                     print()
                     print('"Bestehendesquote"')
+                    print('[x] Zum Hauptmenü')
                     print()
+                    auswahl = input('>>> ' )
+                    
+                    if auswahl == 'x':
+                        print('Du kehrst zum Hauptmenü zurück.')
+                    
+                    if os.name == 'nt':     # Windows 'nt' ist muss
+                       os.system("pause")
+                                                   
+                    else:                     # Linux/macOS
+                        input("Weiter mit Enter...")
+        else:
                     break
+                    print('Ungültige Eingabe')
+                    
     elif auswahl == '9':
         print()
         print ("Programm beendet.Auf Wiedersehen!")
@@ -107,27 +235,33 @@ while True:
 
 
 
-        
-        
+'''schueler_id = int(input('Bitte geben Sie die Schüler id ein 1 -10'))
+          if 0 < or > 100: count(x)
+                      else  print('keine Zahl oder außerhalb von 0 -100')
+punkte_zahl = float(input('Wie viele Punkte möchten Sie eingeben?'))
+          if 0 < or > 100:
+            else  print('keine Zahl oder außerhalb von 0 -100')
+            loop 
+    schüler1 eintragen in Liste / index
+    schüler2
+    schüler3
+    schüler4   -> 10 schleife for schleife
+# Error Handling , valaue keyboard  print('Bitte nur Dezimalzahlen 1-100 im Fomrat 10.1 Eingeben  )
+# Bei überschreitung von gesamt Punkten von 100 print('ERROR: Wert wird nicht übernommern, er liegt nicht im Wertebereich von 0 - 100')
 
-#except ValueError:
-#        print('Bitte "e" DRÜCKEN! e nicht a b oder c einfach "e"')
-#except KeyboardInterrupt:
-#        print("Abbruch durch User")
 
 
 
-'''
-title = str;
-title('Notenverwaltung')
-#Was muss ich deklarieren: import || Feste werte Schüler id &  Name sowie die entsprechenden Listen []
+
+#Was muss ich deklarieren:  Feste werte Schüler id &  Name sowie die entsprechenden Listen []
 namen_schueler = (0 = Leon[], 1 = Mia, 2 = Finn, 3 = Emma, 4 = Noah, 5 = Lena, 6 = Elias, 7 = Ida, 8 = Ben, 9 = Paula)
 name_schueler = str
 schueler_id = int
-wert = float(eingabe.replace(',','.'))
 
+punkte_liste = [[] for _ in range(10)]  # Index 0 = Schüler 1, ... Index 9 = Schüler 10
+'''
 
-
+'''
 #Nummer 1:
 schueler_id = int(input('Bitte geben Sie die Schüler id ein 1 -10'))
           if 0 < or > 100: count(x)
@@ -159,9 +293,6 @@ punkte_zahl = float(input('Wie viele Punkte möchten Sie eingeben?'))
 #noten_durchschnitt = float(input('Der arithmetische Mittelwert: ', mittelwert),2) 
 # print ('"Keine Punkte vorhanden – Durchschnitt nicht berechenbar."')
 
-
-# print ('Back to Menü')
-
 # Nummer 4:
 try:
     punkte_zahl_note = float(input('Bitte geben Sie eine Zahl zwischen 0 -100 ein.:'))
@@ -191,20 +322,11 @@ else:
     else:
         print("Wert zu groß!")
 
-                          
-# print ('Back to Menü')
-
-
-
-
 # Nummer 5:
 # Anzeige der Min & Max Punkte ggf. mitnamen an.
 # Liste leer von schüler id = print('Für diesen Schüler sind noch keine Punkte hinterlegt')
 #except KeyboardInterrupt:
 #        print("Abbruch durch User")
-# print ('Back to Menü')
-
-
 
 # Nummer 6: //Liste noten_zaehler = [0,0,0,0,0,0] für die Noten 1–6
 # print('Notenspiegel')
@@ -213,7 +335,6 @@ else:
 # Note 6: ....
 #except KeyboardInterrupt:
 #        print("Abbruch durch User")
-# print ('Back to Menü')
 
 # Nummer 7:
 Schüler_id = int(input('Bitte geben Sie die Schüler id ein 1 -10'))
@@ -231,112 +352,8 @@ punkte_zahl = float(input('Wie viele Punkte möchten Sie eingeben?'))
 # print ('Der gewünschte zulöschende Wert ist nicht vorhanden')
 #except KeyboardInterrupt:
 #        print("Abbruch durch User")
-# print ('Back to Menü')
-
 
 # Nummer 8:
 # Bestehensquote ?  Schülerzählen wieviele schüler > 50 Punkte
 # Gib aus: "Bestehensquote: 75.0% (6 von 8 Schülern)
-
-#  print ('Back to Menü')
-
-# Nummer 9:erledigt
-#Press E for Programm-'Exit' 
-# except ValueError:
-#        print('Bitte "e" DRÜCKEN! e nicht a b oder c einfach "e"')
-#except KeyboardInterrupt:
-#        print("Abbruch durch User")
-# 
-#    break
-# print ('Back to Menü')
-
-
-
-
-
-
-
-#Muster
-import os
-
-kontostand = 100
-
-while True:
-    # HAUPTMENÜ
-    os.system("clear") # in Windows: "cls"
-    print("Du bist im Hauptmenü")
-    print("Du hast folgende Optionen:")
-    print("[1] Kontostand einsehen")
-    print("[2] Kontostand verändern")
-    print("[x] Programm beenden")
-    auswahl = input(">>> ")
-
-    if auswahl == "1":
-        while True:
-            os.system("clear") # in Windows: "cls"
-            print("Du möchtest deinen Kontostand einsehen")
-            print("Du hast folgende Optionen:")
-            print("[1] Kontostand einsehen")
-            print("[x] Zum Hauptmenü")
-            auswahl = input(">>> ")
-
-            if auswahl == "1":
-                print("Kontostand:", kontostand, "€")
-                os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-            elif auswahl == "x":
-                print("Du kehrst jetzt zum Hauptmenü zurück.")
-                os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-                break
-            else:
-                print("Das verstehe ich nicht.")
-                os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-
-    elif auswahl == "2":
-        while True:
-            os.system("clear") # in Windows: "cls"
-            print("Du möchtest deinen Kontostand verändern")
-            print("Du hast folgende Optionen:")
-            print("[1] Kontostand erhöhen")
-            print("[2] Kontostand verringern")
-            print("[x] Zum Hauptmenü")
-            auswahl = input(">>> ")
-
-            if auswahl == "1":
-                print("Bitte Betrag als Ganzzahl eingeben")
-                try:
-                    betrag = int(input("Betrag: "))
-                except ValueError:
-                    print("Das ist keine valide Eingabe!")
-                    os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-                else:
-                    kontostand = kontostand + betrag
-                    print("Vielen Dank!")
-                    os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-
-            elif auswahl == "2":
-                print("Bitte Betrag als Ganzzahl eingeben")
-                try:
-                    betrag = int(input("Betrag: "))
-                except ValueError:
-                    print("Das ist keine valide Eingabe!")
-                    os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-                else:
-                    kontostand = kontostand - betrag
-                    print("Vielen Dank!")
-                    os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-
-            elif auswahl == "x":
-                print("Du kehrst jetzt zum Hauptmenü zurück.")
-                os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-                break
-            else:
-                print("Das verstehe ich nicht.")
-                os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
-
-    elif auswahl == "x":
-        print("Auf Wiedersehen!")
-        break
-    else:
-        print("Das verstehe ich nicht.")
-        os.system("read -p 'Weiter mit Enter...'") # in Windows: "pause"
 '''
