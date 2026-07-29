54a59978bf56de0d35518b7056336c3c7d2227c1
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-29 21:13:43 +0200

    1

---
+++ ooppython/oop.py
@@ -0,0 +1,12 @@
+# Klassen in Python definieren
+x = 3
+print(type(x))
+# x ist ein Objekt, instanziiert von der Klasse int
+# Ausgabe: <class 'int'>
+
+def f():
+    pass
+print(type(f))
+# f ist ein Objekt, instanziiert von der Klasse function
+# Ausgabe: <class 'function'>
+def c(X)
