8f8117ff12f9b56be36851fd6761c809caa91d98
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-29 23:22:53 +0200

    self

--- ooppython/oop.py
+++ ooppython/oop.py
@@ -1,3 +1,6 @@
+# Kommentieren # oder '''    '''
+# False und True immer Anfang Groß
+
 # Klassen in Python definieren
 x = 3
 print(type(x))
@@ -13,17 +16,40 @@ print(type(f))
 print(type(int))
 print(type(type))
 #########################################################################################################
-class SmartPhone:                                           #CamelCase
-    akkustand = 15
+class SmartPhone:                                         #CamelCase
+  
+    akkuStand = 15
     eingeschaltet = False
     offeneApps = 0
     flugmodus = False         
 
 #print(SmartPhone)
 mein_handy = SmartPhone()
-print('Akkustand',mein_handy.akkustand)
+print('Akkustand',mein_handy.akkuStand)
 print('ist eingeschaltet',mein_handy.eingeschaltet)
 print('offene App"s',mein_handy.offeneApps)
 print('Flugmodus eingeschaltet',mein_handy.flugmodus)
-mein_handy.akkustand = 30                                 # änderung im Objekt
-print('Akkustand',mein_handy.akkustand)
+mein_handy.akkuStand = 30                                 # änderung im Objekt
+print('Akkustand',mein_handy.akkuStand)
+#######################################################################################
+class SmartPhone1:
+      #Konstruktor
+        def __init__(self, akkuStand, eingeschaltet ):  # muss nicht alles kann ist es nie eingeschaltet brauch man diese hier auch nicht../*offeneApps, flugmodus*/..                                
+                                                             
+                                                            #init initialisiert das Objekt mit Werten.
+                                                            # 'self' = this. ist ein Platzhalter für das zukünftige Objekt.' 
+                                                            #self' ist eine Referenz auf aktuelle Instanz (das aktuelle Objekt)
+            self.akkuStand = akkuStand                      # links = objekt eigene AkkuStand ,rechts = Was beim Instanzieren mit gegeben wird. (15, False)
+            self.eingeschaltet = eingeschaltet              # von unten nach oben und dan verteilt.!! Wie immer.
+            self.offeneApps = 0
+            self.flugmodus = False 
+
+
+
+#print(SmartPhone)
+mein_handy1 = SmartPhone1(15, False)
+
+print('Akkustand',mein_handy1.akkuStand)
+print('ist eingeschaltet',mein_handy1.eingeschaltet)
+print('offene App"s',mein_handy1.offeneApps)
+print('Flugmodus eingeschaltet',mein_handy1.flugmodus)
