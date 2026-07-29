8268832273ab442a8686192d240359b4ace39e65
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-29 22:32:38 +0200

    Smartphone

--- ooppython/oop.py
+++ ooppython/oop.py
@@ -3,10 +3,27 @@ x = 3
 print(type(x))
 # x ist ein Objekt, instanziiert von der Klasse int
 # Ausgabe: <class 'int'>
-
+######################################################################################################
 def f():
     pass
 print(type(f))
 # f ist ein Objekt, instanziiert von der Klasse function
 # Ausgabe: <class 'function'>
-def c(X)
+
+print(type(int))
+print(type(type))
+#########################################################################################################
+class SmartPhone:                                           #CamelCase
+    akkustand = 15
+    eingeschaltet = False
+    offeneApps = 0
+    flugmodus = False         
+
+#print(SmartPhone)
+mein_handy = SmartPhone()
+print('Akkustand',mein_handy.akkustand)
+print('ist eingeschaltet',mein_handy.eingeschaltet)
+print('offene App"s',mein_handy.offeneApps)
+print('Flugmodus eingeschaltet',mein_handy.flugmodus)
+mein_handy.akkustand = 30                                 # änderung im Objekt
+print('Akkustand',mein_handy.akkustand)
