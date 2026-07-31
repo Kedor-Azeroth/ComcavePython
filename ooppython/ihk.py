678d40d624904cd6c59e2d84729e978b8e161977
Rene <157251264+Kedor-Azeroth@users.noreply.github.com>
2026-07-31 11:10:49 +0200

    1

---
+++ ooppython/ihk.py
@@ -0,0 +1,41 @@
+#IHK Anforderungen
+'''
+Poloymorphie
+Klasen
+Methoden 
+Attribute
+
+Pseudocode
+
+--------
+Beispiel
++ public
+- Privat
+:Datentyp
+AP2 prüfen
+
+3 getter Mothoden
+
+
+
+
+'''#Erste Aufgabe
+class Value:
+    def __init__(self sensor_id, value, time):
+       self.__sensor_id = sensor_id
+       self.__value = value()
+       self.__time = time()
+       
+    def getid (self) -> int:
+        
+    def getTime(self)        
+        
+   
+   
+    def oneNewVaLue(senosr_id:Integer, value: Double,timestamp:Long)
+    
+    {   # Erstellen (bzw.Instanziieren) einen Value -Objekt:
+        Value v := neu Value (sensor_id, value, timestamp)
+        #Speichern des Value-Objekt
+        tempList.setValue(v);
+    }
